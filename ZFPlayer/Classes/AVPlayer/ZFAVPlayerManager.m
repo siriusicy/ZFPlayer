0b1048556cc6662d1c6974e417d14f75d687eb48
tian_he <tian_he@hunliji.com>
2022-01-19 14:25:18 +0800

    fix视频播放时切到后台播放仍会继续

--- ZFPlayer/Classes/AVPlayer/ZFAVPlayerManager.m
+++ ZFPlayer/Classes/AVPlayer/ZFAVPlayerManager.m
@@ -310,7 +310,7 @@ static NSString *const kPresentationSize         = @"presentationSize";
     [self pause];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         // 如果此时用户已经暂停了，则不再需要开启播放了
-        if (!self.isPlaying && self.loadState == ZFPlayerLoadStateStalled) {
+        if (!self.isPlaying || self.loadState == ZFPlayerLoadStateStalled) {
             self.isBuffering = NO;
             return;
         }
